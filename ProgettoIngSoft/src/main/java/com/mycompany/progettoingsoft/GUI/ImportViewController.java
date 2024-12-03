16673ec28da9b20627ba712e42f028cd68b994a6
andre <andre@172.19.165.226>
2024-12-03 11:08:42 +0100

    Commit della struttura del progetto+Import View

---
+++ ProgettoIngSoft/src/main/java/com/mycompany/progettoingsoft/GUI/ImportViewController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.progettoingsoft.GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+/**
+ * FXML Controller class
+ *
+ * @author andre
+ */
+public class ImportViewController implements Initializable {
+
+    @FXML
+    private ListView<?> directoryListView;
+    @FXML
+    private Button browseButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void readDirectory(ActionEvent event) {
+    }
+    
+}
--- ProgettoIngSoft/src/main/java/com/mycompany/progettoingsoft/IO/FileHandler.java
+++ ProgettoIngSoft/src/main/java/com/mycompany/progettoingsoft/IO/FileHandler.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.progettoingsoft.Rubric;
+package com.mycompany.progettoingsoft.IO;
 
+import com.mycompany.progettoingsoft.Rubric.Rubric;
 import com.mycompany.progettoingsoft.Rubric.Rubric;
 
 /**
