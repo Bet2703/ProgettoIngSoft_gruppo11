541048938eeea8f47a69bb4250eae2ccbeb48f7d
Benedetta <Benedetta@172.19.133.17>
2024-12-06 10:36:27 +0100

    Inserimento commenti doxygen in FileHandler

--- ProgettoIngSoft/src/main/java/com/mycompany/progettoingsoft/IO/FileHandler.java
+++ ProgettoIngSoft/src/main/java/com/mycompany/progettoingsoft/IO/FileHandler.java
@@ -9,10 +9,23 @@ import com.mycompany.progettoingsoft.Rubric.Rubric;
 import com.mycompany.progettoingsoft.Rubric.Rubric;
 
 /**
- *
+ * @file FileHandler.java
+ * @brief questo file contiene un'interfaccia che gestisce le funzionalità "importContacts" e "exportContacts"
+ * 
+ * L'utilizzo di un'interfaccia per queste funzionalità permette di evitare dipendenze tra le classi.
+ * 
  * @author Benedetta
  */
 public interface FileHandler {
-    public abstract void importContacts(String filename);
-    public abstract Rubric exportContacts(String filename);
+    /**
+     * @brief Permette di importare nella rubrica un insieme di contatti.
+     * @param filename 
+     * @return la Rubric importata
+     */
+    public abstract Rubric importContacts(String filename);
+    /**
+     * @brief Permette di esportare la rubrica in un file esterno.
+     * @param filename
+     */
+    public abstract void exportContacts(String filename);
 }
