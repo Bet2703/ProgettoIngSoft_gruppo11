c9babe3a4870323213e4ad7b6b349c644344c17a
Benedetta <Benedetta@172.19.133.17>
2024-12-07 13:03:16 +0100

    Modifiche generali

--- ProgettoIngSoft/src/main/java/com/mycompany/progettoingsoft/Main.java
+++ ProgettoIngSoft/src/main/java/com/mycompany/progettoingsoft/Main.java
@@ -10,10 +10,10 @@ package com.mycompany.progettoingsoft;
  * @author Benedetta
  */
 public class Main {
-
+   
     /**
      * @param args the command line arguments
-     */
+     */        
     public static void main(String[] args) {
         // TODO code application logic here
     }
